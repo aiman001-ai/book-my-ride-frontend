40c9839c597e3d4413eecd440054d428bd53c433
aiman001-ai <aiman001ai@gmail.com>
2025-12-08 13:20:28 +0530

    Frontend UI updates

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
+import { GoogleOAuthProvider } from '@react-oauth/google';
 import 'react-toastify/dist/ReactToastify.css';
 
+const GOOGLE_CLIENT_ID =
+  '42846031442-76iei2p8t06vbnrl7p5mpt4up6rgabvi.apps.googleusercontent.com';
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <GoogleOAuthProvider clientId={GOOGLE_CLIENT_ID}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </GoogleOAuthProvider>
 );
